ab684983bba666675f909740dd6cb2cbb3815827
endulum <71853916+endulum@users.noreply.github.com>
2025-04-13 09:33:26 -0700

    allow checkboxes to edit state

--- src/components/Breeds.tsx
+++ src/components/Breeds.tsx
@@ -1,7 +1,19 @@
-// import { useState } from 'react';
+import { useState, type ChangeEvent } from 'react';
 
 export function Breeds() {
-  // const [breeds, setBreeds] = useState<string[]>([]);
+  const [breeds, setBreeds] = useState<string[]>([
+    'Silver',
+    'Gold',
+    'Staterae',
+  ]);
+
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked === false) {
+      setBreeds(breeds.filter((b) => b !== e.target.name));
+    } else {
+      setBreeds([...breeds, e.target.name]);
+    }
+  };
 
   return (
     <div>
@@ -9,7 +21,13 @@ export function Breeds() {
       <div className="checkboxes">
         {['Silver', 'Gold', 'Staterae'].map((breed) => (
           <div className="checkbox-row" key={breed}>
-            <input type="checkbox" name={breed} id={breed} />
+            <input
+              type="checkbox"
+              name={breed}
+              id={breed}
+              onChange={handleCheck}
+              defaultChecked={true}
+            />
             <label htmlFor={breed}>{breed}</label>
           </div>
         ))}
